29cd9dce78abb296400104ab8fea62ee515cefc4
Matthew Slipper <me@matthewslipper.com>
2020-06-03 15:26:28 -0700

    Always use mini-gmp

--- utils/has_gmp.sh
+++ utils/has_gmp.sh
@@ -11,35 +11,6 @@
 # written to fail if the compiler is linking
 # to mini-gmp instead of gmp.
 
-if test -z "$CC"; then
-  if type gcc > /dev/null 2>& 1; then
-    CC='gcc'
-  elif type clang > /dev/null 2>& 1; then
-    CC='clang'
-  else
-    echo 'false'
-    exit 0
-  fi
-fi
-
-CODE=`
-  echo '#include <stddef.h>'
-  echo '#include <gmp.h>'
-  echo ''
-  echo 'int main(void) {'
-  echo '  mp_limb_t limbs[1] = {0};'
-  echo '  mpz_t x, y;'
-  echo '  mpz_roinit_n(x, limbs, 1);'
-  echo '  mpz_inits(y, NULL);'
-  echo '  mpz_set_ui(y, 3);'
-  echo '  return mpz_jacobi(x, y);'
-  echo '}'
-`
-
-if echo "$CODE" | "$CC" -o /dev/null -lgmp -xc - > /dev/null 2>& 1; then
-  echo 'true'
-else
-  echo 'false'
-fi
+echo 'false';
 
 exit 0
